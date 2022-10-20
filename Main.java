5dded4e97eca4e80b391a0bbceae9cb94bd570b6
Experiment2601 <aniketgavas18@gmail.com>
2022-10-20 23:26:03 +0530

    User_Registration_UC7

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.bridgelabz.userregistration;
 public class Main {
     public static void main(String[] args) {
         UserRegistration user = new UserRegistration();
-        user.check_Password_Atleast_One_UpperCase();
+        user.check_Password_Atleast_One_UpperCase_And_One_Numeric_Value();
     }
 }
 
--- UserRegistration.java
+++ UserRegistration.java
@@ -3,15 +3,15 @@ package com.bridgelabz.userregistration;
 import java.util.Scanner;
 
 public class UserRegistration{
-    public void check_Password_Atleast_One_UpperCase(){
+    public void check_Password_Atleast_One_UpperCase_And_One_Numeric_Value(){
         String password;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your Password: ");
         password = sc.next();
 
-        //Rule 2 - Minimum 8 Characters With At least Minimum 1 Uppercase
-        String regexPassword = "^[A-Z]{1}[a-zA-Z]{7,}$";
+        //Rule 3 - Minimum 8 Characters With At least Minimum 1 Uppercase And Atleast 1 Numeric Value
+        String regexPassword = "^[a-zA-Z0-9]{8,}$";
 
         boolean result = password.matches(regexPassword);
 
@@ -19,7 +19,7 @@ public class UserRegistration{
             System.out.println("Password Is Valid");
         }else{
             System.out.println("Password Is Invalid!!");
-            System.out.println("Please Enter Minimum 8 Characters With Atleast 1 Uppercase Letter");
+            System.out.println("Please Enter Minimum 8 Characters With Atleast 1 Uppercase Letter & Atleast 1 Numeric Number");
         }
     }
 }
