146c1901fc68d18757d6dfbca8cbcdefd6da8c3b
Experiment2601 <aniketgavas18@gmail.com>
2022-10-20 23:16:08 +0530

    User_Registration_UC3

--- UserRegistration.java
+++ UserRegistration.java
@@ -3,21 +3,23 @@ package com.bridgelabz.userregistration;
 import java.util.Scanner;
 
 public class UserRegistration{
-    public void minimumThreeCharacterNameWithFirstLetterIsCapital(){
-        String lastName;
+    public void check_Enter_Email_Validity(){
+        String emailId;
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Last Name: ");
-        lastName = sc.next();
+        System.out.println("Enter Your Email: ");
+        emailId = sc.next();
 
-        String regexLastName = "^[A-Z]{1}[a-z]{2}[a-z]*$";
+        String regexEmailId = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
 
-        boolean result = lastName.matches(regexLastName);
+        boolean result = emailId.matches(regexEmailId);
 
         if(result){
-            System.out.println("It Is Valid Last Name");
+            System.out.println("Email Id Is Valid");
         }else{
-            System.out.println("It Is Invalid!! Please Enter Minimum Three Character Or Start Last Name With Capital");
+            System.out.println("oops Something Went Wrong!!");
+            System.out.println("Please Enter All Required Fields");
+            System.out.println("eg:example.samplemail@gmail.com");
         }
     }
 }
